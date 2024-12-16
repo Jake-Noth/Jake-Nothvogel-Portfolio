26170df2a43f3497c4ec6d2625568433b5670e33
Jake Nothvogel <jacobnothvogel@gmail.com>
2024-12-16 14:49:39 -0600

    feature: static screen on page load

--- src/TVComponents/ScreenRender.tsx
+++ src/TVComponents/ScreenRender.tsx
@@ -1,33 +1,30 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import html2canvas from "html2canvas";
-import ScreensContext from "../screenContext";
 
 interface TVScreenRenderProps {
     screens: JSX.Element[];
+    setScreens: React.Dispatch<React.SetStateAction<HTMLCanvasElement[]>>
 }
 
 export default function ScreenRender(props: TVScreenRenderProps) {
     const [counter, setCounter] = useState(0);
     const renderContainerRef = useRef<HTMLDivElement | null>(null);
-
-    const screensContext = useContext(ScreensContext);
-    const { addScreen } = screensContext || {};
+    const [screens, setScreens] = useState<HTMLCanvasElement[]>([])
 
     const captureScreen = () => {
-        if (addScreen && renderContainerRef.current) {
-            console.log('Capturing screen...');
+        if (renderContainerRef.current) {
             html2canvas(renderContainerRef.current).then((canvas) => {
                 if (canvas) {
                     if (counter < props.screens.length - 1) {
-                        addScreen(canvas);
+                        setScreens((prevScreens) => [...prevScreens, canvas]);
                         setCounter((prevCounter) => prevCounter + 1);
                         console.log('Captured and moving to next screen');
                     } else {
                         setTimeout(() => {
-                            addScreen(canvas);
+                            const tempScreens = [...screens, canvas]
+                            props.setScreens(tempScreens)
                             setCounter((prevCounter) => prevCounter + 1);
-                            console.log('Captured last screen after delay');
-                        }, 2000);
+                        }, 5000);
                     }
                 }
             });
@@ -44,7 +41,7 @@ export default function ScreenRender(props: TVScreenRenderProps) {
         <div
             id="renderContainer"
             ref={renderContainerRef}
-            style={{ height: "100%", width: "100%" }}
+            style={{ height: "100%", width: "100%", borderRadius:"30px" }}
         >
             {counter < props.screens.length ? props.screens[counter] : null}
         </div>
--- src/TVComponents/TVScreen.tsx
+++ src/TVComponents/TVScreen.tsx
@@ -1,46 +1,66 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Screen1 from "../screens/Screen1";
 import Screen2 from "../screens/Screen2";
 import Screen3 from "../screens/Screen3";
 import ScreenRender from "./ScreenRender";
-import ScreensContext from "../screenContext";
-
-
 
 export default function TVScreen() {
 
     const screenComponents = [
-        <Screen1/>,
-        <Screen2/>,
-        <Screen3/>
-    ]
+        <Screen1 />,
+        <Screen2 />,
+        <Screen3 />
+    ];
 
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const screensContext = useContext(ScreensContext)
-    const screens = screensContext?.screens ?? [];
-    console.log('parent')
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [screens, setScreens] = useState<HTMLCanvasElement[]>([]);
+    const staticScreen = useRef<number | null>(null);
 
-    const drawBlankScreen = () => {
-        const canvas = canvasRef.current
-        if(canvas){
-            const ctx = canvas.getContext('2d')
-            if(ctx){
-                ctx.fillStyle = 'black'
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-            }
-        }
+    if (staticScreen.current) {
+        clearInterval(staticScreen.current);
     }
 
-    useEffect(()=>{
-        //drawBlankScreen()
-    },[])
+    function generateNoise(size: number, intensity: number) {
+        const noise = new ImageData(size, size);
+        for (let i = 0; i < noise.data.length; i += 4) {
+            const noiseValue = Math.random() * 255 * intensity;
+            noise.data[i] = noise.data[i + 1] = noise.data[i + 2] = noiseValue;
+            noise.data[i + 3] = 255;
+        }
+        return noise;
+    }
+
+    const loopStaticScreens = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext("2d");
+        if (canvas && ctx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            const noiseTexture = generateNoise(100,0.4)
+            const offscreenCanvas = new OffscreenCanvas(noiseTexture.width, noiseTexture.height);
+            const offscreenCtx = offscreenCanvas.getContext('2d');
+            if (offscreenCtx) {
+                offscreenCtx.putImageData(noiseTexture, 0, 0);
+                const pattern = ctx.createPattern(offscreenCanvas, 'repeat');
+                if (pattern) {
+                    ctx.fillStyle = pattern;
+                    ctx.fillRect(0, 0, canvas.width, canvas.height);
+                }
+            }
+        }
+    };
+
+    useEffect(() => {
+        staticScreen.current = setInterval(loopStaticScreens, 10);
+    }, []);
 
     return (
         <div id="main-screen">
             <canvas id="canvas" ref={canvasRef} />
             {screens.length < screenComponents.length && (
-                <ScreenRender screens={screenComponents} />
+                <ScreenRender screens={screenComponents} setScreens={setScreens} />
             )}
         </div>
     );
 }
+
+
